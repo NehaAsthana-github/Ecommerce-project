16edb1b6ea578c96b6f423be4f5bb4e62e1bf62b
https://github.com/NehaAsthana-github <asthananeha2005@gmail.com>
2023-03-25 21:19:23 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import Nav from "./Nav";
+
+const Header = () => {
+    return (
+        <MainHeader>
+            <NavLink to="/">
+                <img src="./images/logo.png" alt="my logo img" className="imgt"/>
+            </NavLink>
+            <Nav />
+
+        </MainHeader>
+    );
+};
+
+const MainHeader = styled.header`
+padding:0 4.8rem;
+height:8.6rem;
+background-color:${({ theme }) => theme.colors.bg};
+display:flex;
+justify-content: space-between;
+aline-items:center;
+position:relative;
+
+.logo{
+    height:5rem;
+}
+.imgt{
+    margin-Top: 1.18rem
+}
+`;
+
+export default Header;
